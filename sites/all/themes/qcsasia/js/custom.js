cf83389e4b458d91491391b91d45f03bf52d9def
QCS18 <QCS18@PC-18.qcs.taiwan>
2016-03-10 15:53:50 +0800

    Product page many updates

--- sites/all/themes/qcsasia/js/custom.js
+++ sites/all/themes/qcsasia/js/custom.js
@@ -1,4 +1,13 @@
 $(function() {
+    $('.btn-show-hide-text-area').on('click', function () {
+        console.log('totot');
+       if ($('.hidden-text-area').css('display') == 'none') {
+           $(this).find('.glyphicon').removeClass('glyphicon-menu-down').addClass('glyphicon-menu-up');
+       } else {
+           $(this).find('.glyphicon').removeClass('glyphicon-menu-up').addClass('glyphicon-menu-down');
+       }
+       $('.hidden-text-area').slideToggle();
+    });
     $('ul.nav li.dropdown').hover(function() {
         $(this).find('.dropdown-menu').stop(true, true).show();
         $(this).addClass('focus');
--- sites/all/themes/qcsasia/template.php
+++ sites/all/themes/qcsasia/template.php
@@ -39,7 +39,7 @@ function qcsasia_links__system_main_menu($variables) {
         <nav class="navbar navbar-default">
             <div class="container-fluid padding-sm-0">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-main-menu" aria-expanded="false">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span> 
@@ -47,7 +47,7 @@ function qcsasia_links__system_main_menu($variables) {
                     </button>
                     <span class="navbar-brand visible-xs">Menu</span>
                 </div>
-                <div class="navbar-collapse collapse padding-sm-0" id="bs-example-navbar-collapse-1" aria-expanded="false">
+                <div class="navbar-collapse collapse padding-sm-0" id="navbar-collapse-main-menu" aria-expanded="false">
                     <ul class="nav navbar-nav"><?php foreach ($variables['links'] as $link) { ?>
                             <li class="<?= ($link['below'] ? 'dropdown' : '') ?><?= (($_SERVER["REQUEST_URI"] === url($link['link']['link_path']) && $link['link']['href'] != '<front>') ? ' active' : '') ?>">
                                 <a <?= ($link['below'] ? 'role="button" aria-haspopup="true" aria-expanded="false"' : '') ?> href="<?= url($link['link']['link_path']) ?>" >
