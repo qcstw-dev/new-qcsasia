2df38262dc10b0337b3332c403eafbb06e8c6554
QCS18 <QCS18@PC-18.qcs.taiwan>
2016-05-25 12:02:53 +0800

    confirm email page : improve display

--- sites/all/themes/qcsasia/node--confirm_email.tpl.php
+++ sites/all/themes/qcsasia/node--confirm_email.tpl.php
@@ -3,7 +3,7 @@
         <div class="col-xs-12 thumbnail border-none">
             <img src="<?= url(path_to_theme() . "/images/registration/registration-step-3.jpg") ?>" title="step validation member account" alt="step validation member account" />
         </div>
-        <div class="well col-xs-12">
+        <div class="well col-xs-12 text-center">
             <h4>Your e-mail is now confirmed.</h4>
             <p>We will contact you shortly, usually under 24H, to validate your account.</p>
             <p><strong>Your full access to the member area will be granted after validation from our team.</strong></p>
