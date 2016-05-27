116c8806be6e3225f6a908fa2ebd0069fd0c1687
QCS18 <QCS18@PC-18.qcs.taiwan>
2016-05-27 15:24:33 +0800

    Page samples and prototype form request

--- sites/all/themes/qcsasia/template.php
+++ sites/all/themes/qcsasia/template.php
@@ -907,3 +907,55 @@ function displayLogoProcessBlock($aLogoProcess) {
         </div>
     </div><?php
 }
+function displayProductCheckbox($aProducts){
+        foreach ($aProducts as $sId => $aProductOption) { 
+            $oProduct = taxonomy_term_load($sId); ?>
+                <div class="col-xs-6 col-sm-2">
+                    <div class="col-xs-12 thumbnail thumbnail-hover padding-0"><?php
+                        $aLogoProcesses = getLogoProcesses($oProduct);
+                        $sLogoProcessUri = (!$aLogoProcesses 
+                                ? $oProduct->field_main_photo['und'][0]['uri']
+                                : (isset($aLogoProcesses['doming']) && $aLogoProcesses['doming']['thumbnail']
+                                    ? $aLogoProcesses['doming']['thumbnail'] 
+                                    : (isset(array_values($aLogoProcesses)[0]['thumbnail']) && array_values($aLogoProcesses)[0]['thumbnail']
+                                        ? array_values($aLogoProcesses)[0]['thumbnail'] 
+                                        : $oProduct->field_main_photo['und'][0]['uri']))); ?>
+                        <a href="<?= url('taxonomy/term/'.$oProduct->tid) ?>" target="_blank" title="<?= $oProduct->field_product_name['und'][0]['value'] ?>">
+                            <img src="<?= file_create_url($sLogoProcessUri) ?>" title="<?= $oProduct->field_product_name['und'][0]['value'] ?>" alt="<?= $oProduct->field_product_name['und'][0]['value'] ?>" />
+                            <div class="col-xs-12 subtitle-pic"><?= (isset($oProduct->field_product_ref['und'][0]['value']) ? $oProduct->field_product_ref['und'][0]['value'] : '')?></div>
+                        </a><?php
+                        if (isset($aProductOption[$sId.'L'])) { ?>
+                            <div class="col-xs-12 padding-0 margin-bottom-0 border-bottom">
+                                <div class="border-right padding-0 col-xs-2 background-grey text-center">
+                                    <input type="checkbox" name="submitted[product][]" value="<?= $sId.'L' ?>" id="logotyped_<?= $oProduct->field_product_ref['und'][0]['value'] ?>" aria-label="...">
+                                </div>
+                                <div class="col-xs-10">
+                                    <label for="logotyped_<?= $oProduct->field_product_ref['und'][0]['value'] ?>" class="margin-bottom-0 cursor-pointer">Logotyped</label>
+                                </div>
+                            </div><?php
+                        }
+                        if (isset($aProductOption[$sId.'B'])) { ?>
+                            <div class="col-xs-12 padding-0 margin-bottom-0">
+                                <div class="border-right padding-0 col-xs-2 background-grey text-center">
+                                    <input type="checkbox" name="submitted[product][]" value="<?= $sId.'B' ?>" id="blank_<?= $oProduct->field_product_ref['und'][0]['value'] ?>" aria-label="...">
+                                </div>
+                                <div class="col-xs-10">
+                                    <label for="blank_<?= $oProduct->field_product_ref['und'][0]['value'] ?>" class="margin-bottom-0 cursor-pointer">Blank no logo</label>
+                                </div>
+                            </div><?php
+                        } ?>
+                    </div>
+                </div>
+            <?php
+        }
+    }
+    function displaySelectCountry($aCountries, $bIsRequired){ ?>
+        <div class="input-group">
+            <span class="input-group-addon">Country<?= ($bIsRequired ? '*' : '') ?></span>
+            <select class="form-control <?= ($bIsRequired ? 'required' : '') ?>" name="submitted[country]" ><?php
+                foreach ($aCountries as $sIso => $sName) { ?>
+                    <option value="<?= $sIso ?>"><?= $sName ?></option><?php
+                } ?>
+            </select>
+        </div><?php
+    }
---
+++ sites/all/themes/qcsasia/webform-form-46.tpl.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * @file
+ * Customize the display of a complete webform.
+ *
+ * This file may be renamed "webform-form-[nid].tpl.php" to target a specific
+ * webform on your site. Or you can leave it "webform-form.tpl.php" to affect
+ * all webforms on your site.
+ *
+ * Available variables:
+ * - $form: The complete form array.
+ * - $nid: The node ID of the Webform.
+ *
+ * The $form array contains two main pieces:
+ * - $form['submitted']: The main content of the user-created form.
+ * - $form['details']: Internal information stored by Webform.
+ *
+ * If a preview is enabled, these keys will be available on the preview page:
+ * - $form['preview_message']: The preview message renderable.
+ * - $form['preview']: A renderable representing the entire submission preview.
+ */ 
+//$form = drupal_get_form('webform_client_form_46',$node,array()); ?>
+
+<div class="sample-form">
+    <h2>Samples & prototypes</h2>
+    <div class="well font-size-18 text-center">
+        <p>We can send free samples and/or prototypes at your company name.</p>
+        <p>Please, tick items you would like to receive within following list</p>
+    </div>
+    <div class="col-xs-12 padding-0"><?php
+        $bAlertSet = false;
+        $iCountFields = 0;
+        $iCount = 1;
+        foreach ($form['submitted'] as $key => $field) {
+            if (strpos($key, "#") === false && in_array($field['#type'], ['textfield', 'webform_email', 'select'])) { 
+                $iCountFields++;
+            }
+        }
+        foreach ($form['submitted'] as $key => $field) {
+            if (strpos($key, "#") === false) {
+                if  ($field['#webform_component']['form_key'] == 'product') {
+//                  preg_match_all('~([0-9]*)\|(#[A-Z0-9]*)~', $field['#webform_component']['extra']['items'], $match);
+//                  $aProducts = [];
+//                  foreach ($match[0] as $sMatch) {
+//                      list($sKey, $sValue) = explode('|', $sMatch);
+//                      $aProducts[$sKey] = $sValue;
+//                  }
+                    $aProductValues = [];
+                    foreach ($field['#options'] as $sKey => $sValue) {
+                        $aProductValues[substr($sKey, 0, -1)][$sKey] = $sValue;
+                    } ?>
+                    <div class="col-xs-12 padding-0"><?php
+                        displayProductCheckbox($aProductValues); ?>
+                    </div>
+                    <div class="well col-xs-12 font-size-18 text-center">
+                        <p>Complete following form to receive your samples:</p>
+                    </div><?php
+                } 
+                if (in_array($field['#type'], ['textfield', 'webform_email', 'select']) && $field['#webform_component']['form_key'] != 'product') {
+                    if (!$bAlertSet) { ?>
+                        <div class="clearfix"></div>
+                        <div class="alert alert-danger error-message error-message-empty-field">Please inform fields marked in red</div>
+                        <div class="alert alert-danger error-message error-message-email">Please enter a valid email address</div>
+                        <div class="col-md-6"><?php
+                        $bAlertSet = true;
+                    } 
+
+                    if ($field['#type'] == 'select' && $field['#webform_component']['form_key'] == 'country') {
+//                      preg_match_all('~([A-Z]*)\|([a-zA-Z]*)~', $aComponent['extra']['items'], $match);
+                        displaySelectCountry($field['#options'], $field['#attributes']['required']);
+                    } else { ?>
+                        <div class="input-group">
+                            <span class="input-group-addon"><?php print $field['#webform_component']['name'] . ($field['#required'] ? ' *' : '') ?></span>
+                            <input type="<?php print ($field['#type'] === 'webform_email' ? 'email' : 'text') ?>" class="form-control <?= ($field['#type'] === 'webform_email' ? 'email' : 'text') ?> <?php echo ($field['#required'] ? 'required' : '') ?> <?php print ($field['#type'] === 'webform_email' ? 'email' : '') ?>" name="<?php print $field['#name'] ?>" <?php echo ($field['#required'] ? '' : '') ?> />
+                        </div><?php
+                    }
+                    $iCount++; 
+                    if ($iCount == round($iCountFields / 2)) { ?>
+                        </div>
+                        <div class="col-md-6"><?php
+                    }
+                }
+            }
+        } ?>
+        </div>
+        <input type="hidden" name="form_build_id" value="<?= $form['form_build_id']['#value'] ?>">
+        <input type="hidden" name="form_token" value="<?= $form['form_token']['#value'] ?>">
+        <input type="hidden" name="form_id" value="<?= $form['form_id']['#value'] ?>">
+        <div class="col-xs-12">
+            <input class="btn btn-primary btn-submit" type="button" value="Send" />
+        </div>
+    </div>
+    <script>
+        formValidators('sample-form');
+        $('.sample-form .btn-submit').on('click', function () {
+            if (formSubmitValidator('sample-form')) {
+                $('.webform-client-form').submit();
+            }
+        });
+    </script>
+    
+    
