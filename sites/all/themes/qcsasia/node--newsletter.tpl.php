797f719f3699d8dcfc683acfbc033afa673efae1
QCS18 <QCS18@PC-18.qcs.taiwan>
2017-06-08 09:56:53 +0800

    Newsletters page editable in admin

--- sites/all/themes/qcsasia/node--newsletter.tpl.php
+++ sites/all/themes/qcsasia/node--newsletter.tpl.php
@@ -2,16 +2,17 @@
     <h2 class="margin-top-0">Newsletter</h3>
     <div class="col-xs-12 padding-0"><?php
     global $base_url;
-    foreach ($field_newsletter as $field_nl) {
-        $oNewsletter = array_values(entity_load('field_collection_item', [$field_nl['value']]))[0]; ?>
-        <div class="col-sm-3">
-            <a href="<?= str_repeat('{base_url}', $base_url, $oNewsletter->field_newsletter_link['und'][0]['value']) ?>" target="_blank" title="<?= $oNewsletter->field_newsletter_title['und'][0]['value'] ?>">
-                <div class="thumbnail thumbnail-hover">
-                    <img src="<?= file_create_url($oNewsletter->field_newsletter_picture['und'][0]['uri']) ?>" alt="<?= $oNewsletter->field_newsletter_title['und'][0]['value'] ?>" title="<?= $oNewsletter->field_newsletter_title['und'][0]['value'] ?>" />
-                    <div class="subtitle-pic font-size-13"><?= $oNewsletter->field_newsletter_title['und'][0]['value'] ?></div>
-                </div>
-            </a>
-        </div><?php
-    } ?>
+        $vocabulary = taxonomy_vocabulary_machine_name_load('newsletter_block');
+        $aNewsletterBlocks = entity_load('taxonomy_term', FALSE, array('vid' => $vocabulary->vid));
+        foreach ($aNewsletterBlocks as $aNewsletterBlock) { ?>
+            <div class="col-sm-3">
+                <a href="<?= str_replace('{base_url}', $base_url, $aNewsletterBlock->field_newsletter_link['und'][0]['value']) ?>" target="_blank" title="<?= $aNewsletterBlock->field_newsletter_title['und'][0]['value'] ?>">
+                    <div class="thumbnail thumbnail-hover">
+                        <img src="<?= file_create_url($aNewsletterBlock->field_newsletter_image['und'][0]['uri']) ?>" alt="<?= $aNewsletterBlock->field_newsletter_title['und'][0]['value'] ?>" title="<?= $aNewsletterBlock->field_newsletter_title['und'][0]['value'] ?>" />
+                        <div class="subtitle-pic font-size-13"><?= $aNewsletterBlock->field_newsletter_title['und'][0]['value'] ?></div>
+                    </div>
+                </a>
+            </div><?php
+        } ?>
     </div>
 </div>
